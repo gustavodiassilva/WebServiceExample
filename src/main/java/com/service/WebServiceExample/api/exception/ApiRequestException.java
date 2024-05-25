62ac33012b284afd6ec6de7f3442f89a1eff74b0
Gustavo Silva <tavodiassilva@gmail.com>
2024-05-24 23:45:17 -0300

    feat: Create a exception handler

---
+++ src/main/java/com/service/WebServiceExample/api/exception/ApiRequestException.java
@@ -0,0 +1,13 @@
+package com.service.WebServiceExample.api.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
